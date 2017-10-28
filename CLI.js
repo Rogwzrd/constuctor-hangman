7344b7f6362558039ff2fb39cae3c0040dd9a262
Michael Daye <mgdaye@gmail.com>
2017-10-28 09:58:54 -0400

    removed game greeting when you reset the game

--- CLI.js
+++ CLI.js
@@ -1,66 +1,74 @@
 var inquirer = require('inquirer');
 var Letter = require('./letter.js')
 
+var hasStarted = false;
+
 function initGame() {
     var newLetter = new Letter;
 
     function startGame() {
+        if (!hasStarted) {
 
-        inquirer.prompt([
-            {
-                type: "confirm",
-                message: 'Welcome to Constructor Hangman!\n\n Would you like to play',
-                name: "startPage"
-            }
-        ]).then(function (answers) {
-            if (answers.startPage) {
-                newLetter.populateWord();
-                chooseLetter();
-            }
-        });
-    }
-
-    function chooseLetter() {
-
-        inquirer.prompt([
-            {
-                type: "input",
-                message: '\nChoose a letter',
-                name: "letterChoice"
-            }
-        ]).then(function (answers) {
-            if (newLetter.successAndBlank.indexOf("_") != -1 && newLetter.guesses > 0) {
-                newLetter.checkLetter(answers.letterChoice);
-                checkGameState();
-            }
-        });
-    }
-
-    function checkGameState() {
-        if (newLetter.successAndBlank.indexOf("_") != -1 && newLetter.guesses > 0) {
-            chooseLetter()
+            inquirer.prompt([
+                {
+                    type: "confirm",
+                    message: 'Welcome to Constructor Hangman!\n\n Would you like to play',
+                    name: "startPage"
+                }
+            ]).then(function (answers) {
+                if (answers.startPage) {
+                    hasStarted = true
+                    newLetter.populateWord();
+                    chooseLetter();
+                }
+            });
         } else {
-            endGame()
+            newLetter.populateWord();
+            chooseLetter();
+        }
+    }
+
+        function chooseLetter() {
+
+            inquirer.prompt([
+                {
+                    type: "input",
+                    message: '\nChoose a letter',
+                    name: "letterChoice"
+                }
+            ]).then(function (answers) {
+                if (newLetter.successAndBlank.indexOf("_") != -1 && newLetter.guesses > 0) {
+                    newLetter.checkLetter(answers.letterChoice);
+                    checkGameState();
+                }
+            });
         }
 
+        function checkGameState() {
+            if (newLetter.successAndBlank.indexOf("_") != -1 && newLetter.guesses > 0) {
+                chooseLetter()
+            } else {
+                endGame()
+            }
+
+        }
+
+        function endGame() {
+
+            inquirer.prompt([
+                {
+                    type: "confirm",
+                    message: '\nGame Over\nWould you like to play again?',
+                    name: "gameOver"
+                }
+            ]).then(function (answers) {
+                if (answers.gameOver) {
+                    initGame()
+                }
+            });
+        }
+
+        startGame();
     }
 
-    function endGame() {
-
-        inquirer.prompt([
-            {
-                type: "confirm",
-                message: '\nGame Over\nWould you like to play again?',
-                name: "gameOver"
-            }
-        ]).then(function (answers) {
-            if (answers.gameOver) {
-                initGame()
-            }
-        });
-    }
-
-    startGame();
-}
-
-initGame()
+    initGame()
